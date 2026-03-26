c8b50207e9fea0b73efec0ac00b3d08edc787f3b
BarakaLab <85062427+Baraka124@users.noreply.github.com>
2026-03-26 14:36:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const { createApp, ref, reactive, computed, onMounted, watch, onUnmounted } = Vue
 
-    // ============ 1. CONFIGURATION ====----===--====-=
+    // ============ 1. CONFIGURATION ====----===--====-= 
     const CONFIG = {
       API_BASE_URL: window.location.hostname.includes('localhost')
         ? 'http://localhost:3000'
