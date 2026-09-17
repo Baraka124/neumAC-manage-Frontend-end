8db9551dc56c7cc75659d29de88e94d16572b457
BarakaLab <85062427+Baraka124@users.noreply.github.com>
2026-09-17 14:11:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13791,7 +13791,7 @@ document.addEventListener('DOMContentLoaded', () => {
     app.config.errorHandler = (err, instance, info) => {
       console.error('[neumDesk render error]', err, info)
       const viewName = instance?.setupState?.currentView?.value
-      showOnScreenError('Render error' + (viewName ? ' (' + viewName + ' view)' : ''), err, info)
+      showOnScreenError('Render error' + (viewName ? ' (' + viewName + ' view)' : ''), err, info) 
     }
 
     app.mount('#app')
